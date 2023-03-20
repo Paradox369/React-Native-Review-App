2959588a75cd45630f81592e5c0d8524ddba9d31
Paradox369 <george.ketcha@gmail.com>
2023-03-20 09:59:35 +0100

    initial setup

---
+++ screens/about.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function About() {
+  return (
+    <View style={StyleSheet.container}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = styles.create({
+  container: {
+    padding: 24,
+  },
+});
---
+++ screens/reviewDetails.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ReviewDetails() {
+  return (
+    <View style={StyleSheet.container}>
+      <Text>Review Details Screen</Text>
+    </View>
+  );
+}
+
+const styles = styles.create({
+  container: {
+    padding: 24,
+  },
+});
